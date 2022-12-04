f0517fbfd2a21654ebceb61d3179570d65d84593
brian <kipkoechbrian0@gmail.com>
2022-12-04 14:00:24 +0300

    implement jwt auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,33 @@
 class ApplicationController < ActionController::API
+    before_action :authorized
+    def encode_token(payload)
+        JWT.encode(payload, "Forgot to change secret")
+    end
+
+    def auth_header
+        request.headers['Authorization']
+    end
+
+    def decode_token
+        if auth_header
+            token = auth_header.split(" ")[1]
+            JWT.decode(token, "Forgot to change secret", true )
+        rescue JWT::DecodeError
+            nil
+        end
+    end
+    def current_user
+        payload = decode_token
+        if payload
+            user_id = payload[0]['user_id']
+            return User.find_by(id: user_id)
+        end
+        nil
+    end
+    def logged_in
+        !!current_user
+    end
+    def authorized
+        render json: {"error": "Unauthorized"}, status: :unauthorized unless :logged_in
+    end
 end
