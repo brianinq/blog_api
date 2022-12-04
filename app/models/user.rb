d0392649a93ad2cc8a4d79269e315b52dea085aa
brian <kipkoechbrian0@gmail.com>
2022-12-04 14:27:54 +0300

    fix typos

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
     validates :username, uniqueness: {case_Sensitive: false}
-    has_many :blogs, decendant: :destroy
+    has_many :blogs, dependent: :destroy
     has_many :categories, through: :blogs
 end
