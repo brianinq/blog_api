45772cb2232cee802defdedd25f1ee405bda00ff
brian <kipkoechbrian0@gmail.com>
2022-12-04 20:51:25 +0300

    add blog body

--- db/migrate/20221203090449_create_blogs.rb
+++ db/migrate/20221203090449_create_blogs.rb
@@ -3,6 +3,7 @@ class CreateBlogs < ActiveRecord::Migration[7.0]
     create_table :blogs do |t|
       t.string :title
       t.string :poster
+      t.text :body
       t.integer :likes, default: 0
       t.integer :views, default: 1
       t.integer :user_id
