ca87225e81aaf4cdda483302cf60e772b1cc3d50
brian <kipkoechbrian0@gmail.com>
2022-12-04 20:50:42 +0300

    add categories controller methods

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,11 @@
 class CategoriesController < ApplicationController
+    skip_before_action :authorized, only: [:index, :show]
     def index
         render json: Category.all, status: :ok
     end
     def show
         category = Category.find(params[:id])
-        render json: category, include: [:blogs]
+        render json: category, serializer: CategoryBlogsSerializer,status: :ok
     rescue ActiveRecord::RecordNotFound
             render json: {error: "Not Found"}, status: 404
     end
