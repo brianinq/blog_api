9b2910cbd225c457753ceae85208956fa48adb47
brian <kipkoechbrian0@gmail.com>
2022-12-04 14:26:52 +0300

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :categories
-  resources :users
-  resources :blogs
+  resources :categories, only: [:create, :index, :show]
+  resources :users, only: [:create]
+  resources :blogs, only: [:index, :create, :show, :destroy, :update,]
+  post '/login', to: 'auth#create'
+  get '/profile', to: 'users#profile'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
